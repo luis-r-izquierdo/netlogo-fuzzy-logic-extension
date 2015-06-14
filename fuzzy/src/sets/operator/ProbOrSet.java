93d002c56671ee01f113f0f3dcd404a13c6df438
Luis R. Izquierdo <lrizquierdo@ubu.es>
2015-06-14 13:35:00 +0200

    First addition of files

---
+++ fuzzy/src/sets/operator/ProbOrSet.java
@@ -0,0 +1,65 @@
+package sets.operator;
+
+import general.DegreeOfFulfillment;
+
+import java.util.List;
+
+import sets.general.FuzzySet;
+import sets.general.OperatorSet;
+
+/**
+ * This class represents probabilistic-or sets.
+ * 
+ * @author Marcos Almendres.
+ *
+ */
+public class ProbOrSet extends OperatorSet {
+
+	/**
+	 * Call the constructor from FunctionSet.
+	 * 
+	 * @see sets.general.OperatorSet#OperatorSet(String, List, boolean, String,
+	 *      double[])
+	 */
+	public ProbOrSet(List<FuzzySet> params, boolean continuous, String label,
+			double[] universe) {
+		super("Prob-or", params, continuous, label, universe);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public double evaluate(double d) {
+		List<FuzzySet> parameters = getParameters();
+		double result = parameters.get(0).evaluate(d);
+		double b = 0;
+		for (int i = 1; i < parameters.size(); i++) {
+			b = parameters.get(i).evaluate(d);
+			// (x+y - x*y)
+			result = result + (b * (1 - result));
+		}
+		return result;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public double evaluate(FuzzySet f) {
+		if (!f.isContinuous()) {
+			return DegreeOfFulfillment.mixedFulfillment(this, f);
+		} else {
+			return DegreeOfFulfillment.continuousFulfillment(this, f);
+		}
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public String getNLTypeName() {
+		return "Prob-or-set";
+	}
+
+}
